8414408afe36837db01b9a6366c2dc2a3c60017b
Pranav <lifeispranav@gmail.com>
2025-06-25 00:42:36 +0530

    feat: add server from socketio

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 import express from 'express'
-
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+ 
 const port = 3000
 
 const app = express()
+const server = createServer(app)
+
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.send('Hello, World')
+})
+
+io.on('connection', (socket) => {
+  console.log('user Connected:', socket.id)
+})
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
