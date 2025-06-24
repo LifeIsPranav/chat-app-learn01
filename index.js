c50efd73d035be4a291c448fa08b776ff5f7f0a0
Pranav <lifeispranav@gmail.com>
2025-06-25 00:04:45 +0530

    created basic msg send-receive functionality

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
 
 // Maps static file
 app.use('/', express.static(__dirname + '/public'))
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected!', socket.id)
+
+  socket.on('msg_send', (data) => {
+    console.log(data)
+    // io.emit('msg_rcvd', data)
+    socket.emit('msg_rcvd', data)
+  })
+})
+
+server.listen(3000, () => {
   console.log('Server Started at http://localhost:3000')
 })
---
+++ public/script.js
@@ -0,0 +1,17 @@
+var socket = io()
+
+let btn = document.getElementById('btn')
+let inputMsg = document.getElementById('newMsg')
+let msgList = document.getElementById('msgList')
+
+btn.onclick = function exec() {
+  socket.emit('msg_send', {
+    msg: inputMsg.value
+  })
+}
+
+socket.on('msg_rcvd', (data) => {
+  let limsg = document.createElement('li')
+  limsg.innerText = data.msg
+  msgList.appendChild(limsg)
+})
